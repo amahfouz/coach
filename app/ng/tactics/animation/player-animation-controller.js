bcc0784a3e95019a918254f03931280ecc2a5e98
Ayman Mahfouz <amahfouz@gmail.com>
2016-05-30 23:03:09 -0700

    Added required methods to the animation controller.

--- app/ng/tactics/animation/player-animation-controller.js
+++ app/ng/tactics/animation/player-animation-controller.js
@@ -1,10 +1,55 @@
 // controller for player animation
 
-angular.module('coach.animation').controller('AnimationCtrl', function($scope, playerAnimationService) {
+angular.module('coach.animation').controller('AnimationCtrl', function($scope, playerAnimationService, planListService) {
+
+    // animation
 
     $scope.play = function() {
         playerAnimationService.animatePlan(function finalCompletion(err) {
-            console.log("Done " + err);
+            
         });
     };
+
+    $scope.reset = function() {
+        playerAnimationService.reset(function finalCompletion(err) {
+           //
+        });
+    };
+
+    $scope.isAnimateButtonEnabled = function() {
+        return $scope.getPhases().length;
+    };
+
+    $scope.isResetButtonEnabled = function() {
+        return true;
+    };
+
+    // phase manipulation
+
+    $scope.addAnimationPhase = function(){
+        playerAnimationService.addPhase();
+    };
+
+    $scope.deleteAnimationPhase = function(index) {
+        playerAnimationService.deletePhase();
+    };   
+
+    $scope.selectPhase = function(index) {
+        playerAnimationService.setSelectedPhase(index);
+    };
+
+    $scope.getSelectedPhase = function() {
+        return playerAnimationService.getSelectedPhase();
+    };
+
+    $scope.getPhases = function() {
+        var selPlan = planListService.getSelectedPlan();
+        if (! selPlan) {
+            $log.log("No selected plan.");
+            return;
+        }
+
+        return selPlan.animation;
+    };
 });
+
