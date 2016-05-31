26f0beb3c3824dad7875efda1591f234b3bf5c49
Ayman Mahfouz <amahfouz@gmail.com>
2016-05-30 23:05:09 -0700

    Removed inlined template from the floating panel.

--- app/ng/util/floating-panel.js
+++ app/ng/util/floating-panel.js
@@ -13,7 +13,6 @@ angular.module('coach.panels', ['ngSanitize'])
       'content': 'paneContent',
       'footer' : 'paneFooter'
     },
-    //template: '<div class="panel panel-primary">  <div class="panel-heading cursor-move"> <span> <big>Titllllle</big> </span> </div> <div id="panelBody" class="panel-collapse collapse in"> <section  ng-transclude="contentSot">Empty</section> Heeeye <div ng-transclude="footerSlot">Ayman</div> </div>  </div>'
     templateUrl: "ng/util/floating-panel.tpl.html"
   };
 });
