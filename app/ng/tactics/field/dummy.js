c92ecd8aafc10c94390120a39978c2aafa2103a4
Ayman Mahfouz <amahfouz@gmail.com>
2016-06-10 00:18:05 -0700

    Removed unused dummy file.

--- app/ng/tactics/field/dummy.js
+++
@@ -1,9 +0,0 @@
-// dummy file whose only purpose is to define variables referenced 
-// globally from other files so that the gulp lint task does not
-// report those references as errors.
-
-// JQuery
-var $ = {};
-
-// lodash
-var _ = {};
